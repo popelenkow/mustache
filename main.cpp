292bf695744929d7c4a270df29929363550f6696
Popelenkov Vladlen <popelenkow@yandex.ru>
2014-11-25 20:59:35 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include "mustache.h"
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+std::ifstream i_open(const std::string &str)
+{
+	std::ifstream input(str);
+	if ( !input )
+	{
+		std::string buf = "Not opened \"" + str + '"';
+		throw buf;
+	}
+	return input;
+}
+
+std::ofstream o_open(const std::string &str)
+{
+	std::ofstream input(str);
+	if ( !input )
+	{
+		std::string buf = "Not opened \"" + str + '"';
+		throw buf;
+	}
+	return input;
+}
+
+int main ()
+{
+	try
+	{
+		std::ifstream &in_database = i_open("database.txt");
+		std::ifstream &in_text = i_open("input.txt");
+		std::ofstream &out_text = o_open("output.txt");
+		
+		Mustache mustache;
+		mustache.go(in_database, in_text, out_text);
+		
+		in_database.close();
+		in_text.close();
+		out_text.close();
+	}
+	catch (std::string err)
+	{
+		std::cerr << err << std::endl;
+	}
+
+	
+	return 0;
+}
