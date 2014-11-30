c666fbb76b177ad18d09c69f37ee35c9caa9caae
Popelenkov Vladlen <popelenkow@yandex.ru>
2014-12-01 00:02:54 +0700

    Update translator.h

--- translator.h
+++ translator.h
@@ -1,23 +1,19 @@
 #pragma once
-
 #include "hash.h"
-
-
 #include <iostream>
 #include <sstream>
-
 class Traslator
 {
 	bool check_condition(std::stringstream &input, HashDatabase *&it_database)
 	{
 		std::string buf;
-		for(;;)
+		for (;;)
 		{
-			getline(input, buf, '.') ;
-			if( input.eof() )
+			getline(input, buf, '.');
+			if (input.eof())
 			{
-				if ( buf.empty() ) return true;
-				else if ( it_database->this_exists(buf) )
+				if (buf.empty()) return true;
+				else if (it_database->this_exists(buf))
 				{
 					it_database = it_database->get_next(buf);
 					return true;
@@ -25,7 +21,7 @@ class Traslator
 				else return false;
 			}
 			it_database = it_database->get_next(buf);
-			if ( it_database == NULL ) return false;
+			if (it_database == nullptr) return false;
 		}
 	}
 	std::string get_ready_text(HashText *text_inside)
@@ -37,31 +33,26 @@ class Traslator
 		}
 		return buf;
 	}
-	
-
 	void go_condition(HashDatabase *database_inside, HashText *text_inside, std::vector<int> &condition, bool is_positive)
 	{
 		for (int i = 0; i < condition.size(); i++)
 		{
 			int pos = condition[i];
 			HashText::Cell &it = text_inside->box[pos];
-
-			std::stringstream input( it.first );
+			std::stringstream input(it.first);
 			HashDatabase *it_database = database;
-
 			it.first.clear();
-			if ( is_positive == check_condition(input, it_database) )
+			if (is_positive == check_condition(input, it_database))
 			{
-				if ( it_database != NULL ) it.first += it_database->boards.left;
+				if (it_database != nullptr) it.first += it_database->boards.left;
 				it.first += go(it_database, it.second);
-				if ( it_database != NULL ) it.first += it_database->boards.right;
+				if (it_database != nullptr) it.first += it_database->boards.right;
 			}
 		}
 	}
-
-	std::string go_data (HashDatabase *database_inside, HashText *text_inside)
+	std::string go_data(HashDatabase *database_inside, HashText *text_inside)
 	{
-		if ( database_inside == NULL)
+		if (database_inside == nullptr)
 		{
 			for (int i = 0; i < text_inside->data.size(); i++)
 			{
@@ -73,7 +64,7 @@ class Traslator
 		}
 		else
 		{
-			if ( text_inside->data_is_array )
+			if (text_inside->data_is_array)
 			{
 				std::vector<std::string> buf;
 				text_inside->data_is_array = false;
@@ -86,11 +77,10 @@ class Traslator
 				std::string str;
 				for (auto it = database_inside->box.begin(); it != database_inside->box.end(); it++)
 				{
-					if ( it != database_inside->box.begin()) str += '\n';
-					if ( it->second != NULL) str += it->second->boards.left;
+					if (it != database_inside->box.begin()) str += '\n';
+					if (it->second != nullptr) str += it->second->boards.left;
 					str += go_data(it->second, text_inside);
-					if ( it->second != NULL) str += it->second->boards.right;
-
+					if (it->second != nullptr) str += it->second->boards.right;
 					for (int i = 0; i < text_inside->data.size(); i++)
 					{
 						int pos = text_inside->data[i];
@@ -106,20 +96,16 @@ class Traslator
 				{
 					int pos = text_inside->data[i];
 					HashText::Cell &it = text_inside->box[pos];
-					it.first = database_inside->get_data( it.first );
+					it.first = database_inside->get_data(it.first);
 				}
 				return get_ready_text(text_inside);
 			}
 		}
-		
 	}
-
-
 	std::string go(HashDatabase *database_inside, HashText *text_inside)
 	{
 		go_condition(database_inside, text_inside, text_inside->positive, true);
 		go_condition(database_inside, text_inside, text_inside->negative, false);
-		
 		return go_data(database_inside, text_inside);
 	}
 	HashDatabase *database;
